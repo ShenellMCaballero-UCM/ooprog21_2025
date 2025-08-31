071327813c14f63f0e3801609e111a55d8869a62
ShenellMCaballero-UCM <shenmcaballero@gmail.com>
2025-08-31 17:42:21 +0800

    Create FirstDialog.java

---
+++ chapter_1/FirstDialog.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+ public class FirstDialog
+ {
+ 
+ public static void main (String [] args)
+ {
+ 
+   JOptionPane.showMessageDialog(null, "First java dialog"," Message",JOptionPane.INFORMATION_MESSAGE );
+   
+ }
+   
+ }
