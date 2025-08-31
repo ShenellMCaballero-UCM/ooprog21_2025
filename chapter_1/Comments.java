7b6a64ad7173ae535a6dfa3c28f76363171a8a47
ShenellMCaballero-UCM <shenmcaballero@gmail.com>
2025-08-31 17:43:57 +0800

    Create Comments.java

---
+++ chapter_1/Comments.java
@@ -0,0 +1,12 @@
+// the single line
+public class Comments  
+{
+    public static void main(String[] args)
+    {
+      
+        /* the mutli-line */
+        System.out.println("Hi, this is comment");
+      
+        /** the documentation */
+    }
+}
