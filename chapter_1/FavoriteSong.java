e3b84c1369d3f737995c2732bf8739db67ed8771
ShenellMCaballero-UCM <shenmcaballero@gmail.com>
2025-08-31 17:49:17 +0800

    Create FavoriteSong.java

---
+++ chapter_1/FavoriteSong.java
@@ -0,0 +1,11 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("i just wanna be yours");
+        System.out.println("secrets I have held in my heart");
+        System.out.println("Are harder to hide than I thought");
+        System.out.println("Maybe i just wanna be yours, I wanna be yours");
+        System.out.printIn("Wanna be yours");
+        System.out.printIn("Wanna be yours");
+        System.out.printIn("Wanna be yours");
+    }
+}
