892038fd1b65dade1cd38ec40be6984b7dc4ae9a
ShenellMCaballero-UCM <shenmcaballero@gmail.com>
2025-09-19 08:43:39 +0800

    Add files via upload

---
+++ Chapter_3/DemoGrossPay.java
@@ -0,0 +1,17 @@
+class DemoGrossPay{
+
+public static void calculateGross(double hoursWorked)
+{
+
+final double HOURLY_RATE = 22.75;
+double grossPay = hoursWorked * HOURLY_RATE;
+System.out.println(hoursWorked + " hours at $" + HOURLY_RATE + "per hour is $" + grossPay);
+}
+
+public static void main(String[] args)
+{
+calculateGross(10.0);
+calculateGross(25.0);
+calculateGross(37.5);
+}
+}
