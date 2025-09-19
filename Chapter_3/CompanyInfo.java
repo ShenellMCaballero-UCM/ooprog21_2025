404e2b6d5b6e27ce2f12e2d7c87eafae44d7f599
ShenellMCaballero-UCM <shenmcaballero@gmail.com>
2025-09-19 08:50:13 +0800

    Create CompanyInfo.java

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,17 @@
+class CompanyInfo
+{
+
+      public static void main (String[] args) 
+{
+      System.out.println("TechWorld Computer");
+      displayHours();
+}
+      
+      public static void displayHours() 
+{
+      System.out.println("Monday - Friday 8 am to 6pm");
+      System.out.println("Saturday 8 am to noon");
+      System.out.println("Sunday closed");
+}
+}
+   
