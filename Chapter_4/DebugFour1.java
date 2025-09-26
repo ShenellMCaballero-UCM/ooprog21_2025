8ba1e407d990e20ec6433f1e03fddea5d7e3fc5f
ShenellMCaballero-UCM <shenmcaballero@gmail.com>
2025-09-26 09:43:18 +0800

    DebugFour1.java

---
+++ Chapter_4/DebugFour1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DebugFour1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a radius for a circle >> ");
+        int radius = input.nextInt();
+
+        DebugCircle c = new DebugCircle(radius);
+
+        System.out.println("The radius is " + c.getRad());
+        System.out.println("The diameter is " + c.getDiam());
+        System.out.println("The area is " + c.getArea());
+    }
+}
